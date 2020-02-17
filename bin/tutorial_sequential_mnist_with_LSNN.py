cec2266b5707c699bb297ed8bd56c278fefde8ce
Guillaume Bellec <guillweb@gmail.com>
2020-02-17 14:59:30 +0100

    Update tutorial_sequential_mnist_with_LSNN.py

--- bin/tutorial_sequential_mnist_with_LSNN.py
+++ bin/tutorial_sequential_mnist_with_LSNN.py
@@ -1,4 +1,16 @@
 '''
+The purpose of this script is to test the ALIF cell model and
+provide a basic tutorial.
+
+Out of the box you should get good performance (it uses a 20%
+rewiring), please report any bug or unexpected performance.
+
+One should get approximately:
+- 40% accuracy in 100 iterations
+- 60% in 200 iterations (about 30 minutes in our CPUs and faster with GPUs)
+- you should eventually get above 90% with 20k ~ 30k iterations.
+Successful runs should achieve up to 96% in 36k iterations (takes ~24h on GTX1080).
+
 The Clear BSD License
 
 Copyright (c) 2019 the LSNN team, institute for theoretical computer science, TU Graz
@@ -10,18 +22,6 @@ Redistribution and use in source and binary forms, with or without modification,
 * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 * Neither the name of LSNN nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-The purpose of this script is to test the ALIF cell model and
-provide a basic tutorial.
-
-Out of the box you should get good performance (it uses a 20%
-rewiring), please report any bug or unexpected performance.
-
-One should get approximately:
-- 40% accuracy in 100 iterations
-- 60% in 200 iterations (about 30 minutes in our CPUs and faster with GPUs)
-- you should eventually get above 90% with 20k ~ 30k iterations.
-Best runs should achieve up to 96% in 36k iterations (takes ~24h on GTX1080).
 '''
 
 from lsnn.toolbox.tensorflow_einsums.einsum_re_written import einsum_bij_jk_to_bik
